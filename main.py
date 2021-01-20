b90ca5fa437b07a0dd892c446774d08e71a25b7d
Shihao Cao <shehowcow@gmail.com>
2021-01-20 02:16:29 -0500

    try go conversion

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import logging
 import coloredlogs
 
 from Coach import Coach
-from othello.OthelloGame import OthelloGame as Game
-from othello.pytorch.NNet import NNetWrapper as nn
+from go.OthelloGame import OthelloGame as Game
+from go.tensorflow.NNet import NNetWrapper as nn
 from utils import *
 
 log = logging.getLogger(__name__)
